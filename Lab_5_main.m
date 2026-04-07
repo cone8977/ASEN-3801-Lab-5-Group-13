560cb751510f8cba6ee9ff3f2b99515243129dc7
cone8977 <130427347+cone8977@users.noreply.github.com>
2026-04-07 08:58:04 -0600

    Updated formatting

--- Lab_5_main.m
+++ Lab_5_main.m
@@ -1,9 +1,18 @@
+% Contributors: Tyler Emmons, Wang Long Lee, Cody Newton, Carson Sutton 
+% Course number: ASEN 3801
+% File name: PlotAircraftSim
+% Created: 4/7/26
+
 clc;
 clear;
 close all;
 
-
-
-%% Aircraft Parameter structure 
-
+% Aircraft Parameter structure 
 a_params=ttwistor();
+
+
+Velocity=1; %[m/s]
+[wind_angles] = WindAnglesFromVelocityBody(Velocity);
+
+
+
